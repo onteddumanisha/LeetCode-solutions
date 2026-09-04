4cdfec1eb242036393d73973c912d6918bf5b6ed
onteddumanisha <onteddumanisha@gmail.com>
2026-09-04 11:54:28 +0530

    Time: 48 ms (30.65%), Space: 19.4 MB (7.21%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -12,10 +12,11 @@ class Solution:
         while left <= right:
             mid = left + (right-left)//2
             result = guess(mid)
+            
             if result == 0:
                 return mid
             elif result == 1:
-                left = mid+1
+                left = mid + 1
             else:
                 right = mid - 1
         
