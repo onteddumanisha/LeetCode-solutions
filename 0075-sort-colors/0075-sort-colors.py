12174286b33045c4ad3e679c6d1af56fa1fe25b8
onteddumanisha <onteddumanisha@gmail.com>
2026-09-12 12:25:02 +0530

    Time: 3 ms (11.73%), Space: 19.3 MB (62.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,7 +2,7 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         n = len(nums)
         for i in range(n-1):
-            for j in range(n-1):
+            for j in range(n-1-i):
                 if nums[j] > nums[j+1]:
                     nums[j],nums[j+1] = nums[j+1],nums[j]
         return nums
